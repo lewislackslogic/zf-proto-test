41f5d7a371e71ad0b1346b0a788ae016a345c01e
Lewis O'Dea <lewisodea@live.co.uk>
2018-10-20 22:53:33 +0100

    First Commit

---
+++ src/pages/faqs/faqs.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-faqs',
+  templateUrl: 'faqs.html'
+})
+export class FAQsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
